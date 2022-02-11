26649c2baa47a31ae63abbc17b3bdc2fdae62a20
shikhar0512L <74178918+shikhar0512L@users.noreply.github.com>
2022-02-11 09:36:50 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,38 @@
+import { SafeAreaView, View, Image } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from "@env";
+
+const HomeScreen = () => {
+   // console.log(GOOGLE_MAPS_APIKEY)
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{ width: 100, height: 100, resizeMode: "contain" }}
+                    source={{
+                        uri: "https://links.papareact.com/gzs"
+                    }}
+                />
+
+                <GooglePlacesAutocomplete
+                    style={{
+
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    placeholder='Where from?'
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                />
+                {/* <NavOptions/> */}
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
---
+++ screens/MapScreen.js
@@ -0,0 +1,13 @@
+import {Text, View } from 'react-native';
+import React from 'react';
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is the map stuff</Text>
+
+    </View>
+  );
+};
+
+export default MapScreen;
